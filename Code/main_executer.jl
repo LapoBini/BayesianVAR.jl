6d533270b7452712f460bbf1350fe39c3357d492
LapoBini <lapo.bini96@gmail.com>
2024-09-19 11:16:36 -0700

    .

--- Code/main_executer.jl
+++ Code/main_executer.jl
@@ -1,5 +1,15 @@
 # ------------------------------------------------------------------------------
 # EXECUTER BVAR
 # ------------------------------------------------------------------------------
+# Set-up of the model 
 include(pwd()*"/Code/SetUp/SetupBVAR.jl");
-bvar_main(data_path, start_date, end_date, results_folder)
+
+# run the model 
+bvar_main(data_path, start_date, end_date, results_folder,
+# If you want set manually the hyperpriors 
+λ  = [0.1,1.0,10.0,100],
+τ  = [0.1,1.0,10.0,100].*10,
+γ  = [0.1,1.0,10.0,100].*10,
+ε  = 0.0001,
+p  = [2,3,4,5,6,7,8,12]
+)

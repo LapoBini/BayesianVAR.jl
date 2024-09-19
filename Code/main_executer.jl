45af82dc21ea2f82c84d15d99fa4af296f40b397
LapoBini <lapo.bini96@gmail.com>
2024-09-19 11:19:22 -0700

    .

--- Code/main_executer.jl
+++ Code/main_executer.jl
@@ -8,8 +8,8 @@ include(pwd()*"/Code/SetUp/SetupBVAR.jl");
 bvar_main(data_path, start_date, end_date, results_folder,
 # If you want set manually the hyperpriors 
           λ = [0.1,1.0,10.0,100],     # shrinkage minnesota prior
-          τ = [0.1,1.0,10.0,100].*10, # shrinkage sum-of-coefficients 
-          γ = [0.1,1.0,10.0,100].*10, # shrinkage dummy-initial-observations 
+          τ = [0.1,1.0,10.0,100].*10, # shrinkage sum-of-coefficients ptiot 
+          γ = [0.1,1.0,10.0,100].*10, # shrinkage dummy-initial-observations prior 
           ε = 0.0001,                 # diffuse prior intercept
           p = [2,3,4,5,6,7,8,12]      # lag-length BVAR 
           )
